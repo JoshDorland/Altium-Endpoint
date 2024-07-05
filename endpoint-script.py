18f08e496018c29283eba17427e4e83a04d7b985
JoshDorland <joshdorland@icloud.com>
2024-07-04 21:33:44 -0700

    create .NET, read .NET

--- endpoint-script.py
+++ endpoint-script.py
@@ -1,1 +1,16 @@
-print("hello world")
+file_path = 'endpoint_script.NET'
+
+try:
+    file_object = open(file_path, 'r')
+
+    for line in file_object:
+        if (line.read(6) == '[00001]'):
+            print("true")
+        else:
+            print("false")
+
+        print(line, end ='')
+finally:
+    file_object.close()
+
+
