c99b3d8dfcbf9ccb44747fd22a17ab73560381c4
JoshDorland <joshdorland@icloud.com>
2024-07-06 19:55:35 -0700

    start net class

--- endpoint-script.py
+++ endpoint-script.py
@@ -1,16 +1,43 @@
-file_path = 'endpoint_script.NET'
+netlist_path = 'endpoint_script.NET' #TODO: Make this an actual path somewhere
+net_nums = 0 #counter for the total number circuit nets
 
-try:
-    file_object = open(file_path, 'r')
+class Net:
+    def __init__(self, num, ckt, harn, col, size, twist, shld):
+        self.num = num
+        self.ckt = ckt
+        self.harn = harn
+        self.col = col
+        self.size = size
+        self.twist = twist
+        self.shld = shld
+    
+ 
+net_count = ['[', '0', '0', '0', '2', '0', ']']
 
-    for line in file_object:
-        if (line.read(6) == '[00001]'):
-            print("true")
-        else:
-            print("false")
 
-        print(line, end ='')
-finally:
-    file_object.close()
+for i in range(0, 7):
+    print(net_count[i])
+
+
+print(net_count)
+
+# try:
+#     netlist_object = open(netlist_path, 'r')
+#     line = netlist_object.readline()
+
+#     while line != '':
+#         line_list = list(line)
+#         print(line_list, end='\n')
+
+#         if line_list[0] == '[':
+#             print("true")
+#             break 
+    
+#         line = netlist_object.readline()
+        
+
+
+# finally:
+#     netlist_object.close()
 
 
