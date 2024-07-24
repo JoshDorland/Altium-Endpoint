a8a0eeba0550bdca771e81d51e019c26c4b32ed1
JoshDorland <joshdorland@icloud.com>
2024-07-23 22:06:39 -0700

    Add minor error handling

--- endpoint-script.py
+++ endpoint-script.py
@@ -2,7 +2,7 @@ netlist_path = 'endpoint_script.NET' #TODO: Make this an actual path somewhere
 net_nums = 0 #counter for the total number circuit nets
 
 class Net:
-    def __init__(self, num, crkt, harn, col, size, twst=None, shld=None, from_conn=None, to_conn=[]):
+    def __init__(self, num, crkt, harn, col, size, twst=None, shld=None, from_conn=[None, None], to_conn=[None, None]):
         self.num = num
         self.crkt = crkt
         self.harn = harn
@@ -17,14 +17,20 @@ class Net:
         about_net = [self.num, self.crkt, self.harn, self.col, self.size, self.twst, self.shld, self.from_conn, self.to_conn]
         return about_net
     
-    def connectNets(self, to_conn):
-        self.to_conn.append(to_conn)
+    def connectNets(self, from_conn=[None, None], to_conn=[None, None]):
+        if from_conn[0] == "*" or to_conn[0] == "*":
+            raise Exception("Designator missing for" + ' ' + self.crkt)
+        else:
+            self.from_conn = from_conn
+            self.to_conn = to_conn
 
-net_1 = Net(1, "GND-01", "RH", "RD", 0.35, from_conn="X013")
+net_1 = Net(1, "GND-01", "RH", "RD", 0.35)
 print(net_1.aboutNet())
 
-net_1.connectNets("X022")
+net_1.connectNets(["X013", 8], ["*", 4])
 print(net_1.aboutNet())
+
+
     
 # for i in range(0, 7):
 #     print(net_count[i])
